d232d45dcc4bc1f948bcc9146b60fcb75656f3c0
Sky Rose <zgnomdnv@sky.skyqrose.com>
2019-11-28 05:31:38 -0500

    serve transparent tile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from PIL import Image
+import io
 
 class request_handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -8,6 +10,8 @@ class request_handler(BaseHTTPRequestHandler):
                 self._write_data(200, file)
             except:
                 self._write_data(500, "500")
+        if self.path == "/tile":
+            self._write_data(200, tile())
         else:
             self._write_data(404, "404")
 
@@ -17,6 +21,13 @@ class request_handler(BaseHTTPRequestHandler):
         self.send_response(status)
         self.end_headers()
         self.wfile.write(data)
+
+def tile():
+    img = Image.new("RGBA", (255, 255), color = (0, 0, 0, 0))
+    imgBytes = io.BytesIO()
+    img.save(imgBytes, format="PNG")
+    return imgBytes.getvalue()
+
 
 if __name__ == "__main__":
     httpd = HTTPServer(('localhost', 6371), request_handler)
